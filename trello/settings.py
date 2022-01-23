8ab9910bc553ce620f63ae33e793c5ca732dcdee
Jennifer Roberts | Studydrive <RoberJ01@stepstone.com>
2022-01-23 14:21:00 +0100

    remove frontend from app list

--- trello/settings.py
+++ trello/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'rest_framework',
-    'frontend'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
