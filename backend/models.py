62dbd4608bc146e402ac9b005cfe7d4e65fdbd28
Jangle <jangle282@gmail.com>
2022-02-19 19:22:08 +0100

    make card description nullable

--- backend/models.py
+++ backend/models.py
@@ -12,7 +12,7 @@ class Column(models.Model):
 
 class Card(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     column = models.ForeignKey(Column, related_name='cards', on_delete=models.CASCADE)
     order = models.IntegerField(default=-1)
 
