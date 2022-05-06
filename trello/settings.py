473ddeb88fc817a8b37296e751e13b1c8c14f4a8
Jangle <jangle282@gmail.com>
2022-05-06 10:01:39 +0200

    remove key to env file and change

--- trello/settings.py
+++ trello/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from subprocess import _ENV
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ss*tsxmayj90c0ack_a+%0^cd2tv&_=l8&di_)by24!&-=hp8='
+SECRET_KEY = _ENV('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
